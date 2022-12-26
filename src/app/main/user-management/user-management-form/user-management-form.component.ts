82230fdc8dc5d7fc357bb34e67141d865cf9ec41
anhvhn <anhvhn@fpt.com.vn>
2022-12-26 18:28:27 +0700

    form and table

--- src/app/main/user-management/user-management-form/user-management-form.component.ts
+++ src/app/main/user-management/user-management-form/user-management-form.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+class ImageSnippet {
+  pending: boolean = false;
+  status: string = 'init';
+  constructor(public src: string, public file: File) {}
+}
 
 @Component({
   selector: 'app-user-management-form',
   templateUrl: './user-management-form.component.html',
   styleUrls: ['./user-management-form.component.scss']
 })
-export class UserManagementFormComponent {
+export class UserManagementFormComponent implements OnInit {
+
+  customStylesValidated = false;
+  browserDefaultsValidated = false;
+  tooltipValidated = false;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onSubmit3() {
+    this.tooltipValidated = true;
+    console.log('Submit... 3');
+  }
+
+  onReset3() {
+    this.tooltipValidated = false;
+    console.log('Reset... 3');
+  }
+
 
 }
